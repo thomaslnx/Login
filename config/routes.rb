045cf10e95929289c302de092ae0f1590663e6a2
thomaslnx <marcos.msilva10@gmail.com>
2018-12-31 19:59:03 -0300

    Signup finalizado

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  #get 'users/new'
+  # get 'users/show'
+  # get 'users/new'
   get 'static_pages/home'
   get 'static_pages/sobre'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   #root 'application#login'
   root 'static_pages#home'
+  resources :users
 end
